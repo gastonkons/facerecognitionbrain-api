abd18bb821dec6f9ce9e47ceacdd09e67634bf0f
gastonkons <gastonkons@gmail.com>
2020-07-19 18:29:39 -0300

    add host

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
-    user: 'postgres',
-    password: 'dionisio',
+    host: 'postgresql-metric-61139',
+    user: 'gastonkons',
+    password: '',
     database: 'smartbrain'
   }
 });
